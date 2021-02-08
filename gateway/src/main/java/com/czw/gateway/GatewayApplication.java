e258ef674081bf1fb93ccccc441e2dacb74d78b4
chenziwen <401030526@qq.com>
2021-02-08 15:14:12 +0800

    1.添加了gateway模块，gate模块要求真多，不能和starter-web一起，而且对springboot版本和springcloud版本也有要求

--- gateway/src/main/java/com/czw/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/czw/gateway/GatewayApplication.java
@@ -1,13 +1,17 @@
 package com.czw.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GatewayApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
     public static void main(String[] args) {
+        LOG.info("服务模块开始启动");
         SpringApplication.run(GatewayApplication.class, args);
+        LOG.info("服务模块启动成功: http://localhost:9001");
     }
-
 }
